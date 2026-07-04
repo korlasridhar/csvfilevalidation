e7b5352359fbeea37b9effc533ef423b0721dae4
sridhar <kvvsri@gmail.com>
2026-07-04 16:38:59 -0700

    added new testcases and updated the code

--- tests/test_filedata.py
+++ tests/test_filedata.py
@@ -1,22 +1,20 @@
 import pytest
 
-import pandasql as ps
+from  pandasql import sqldf
 from src.data_connection.csv_reader import get_csv_data
 from src.data_comparison.compare_data import compare_df
 
+source_df = get_csv_data(r'C:\Users\sridh\PycharmProjects\csvFileValidation\data\source.csv')
+target_df = get_csv_data(r'C:\Users\sridh\PycharmProjects\csvFileValidation\data\target.csv')
 
 def test_filedata():
-    source_df = get_csv_data(r'C:\Users\sridh\PycharmProjects\csvFileValidation\data\source.csv')
-    target_df = get_csv_data(r'C:\Users\sridh\PycharmProjects\csvFileValidation\data\target.csv')
     result = compare_df(source_df, target_df)
     assert result
 
 def test_filedata1():
-    source_df = get_csv_data(r'C:\Users\sridh\PycharmProjects\csvFileValidation\data\source.csv')
-    target_df = get_csv_data(r'C:\Users\sridh\PycharmProjects\csvFileValidation\data\target.csv')
-    result = compare_df(source_df, target_df)
-    assert result
+    result = sqldf("select age from source_df where age < 30")
+    assert result['age'].iloc[0] ==20
 
-def test_branch1_update():
-    print('Conflict Scenario')
-    pass
+def test_filedata2():
+    result = sqldf("select age from source_df where age < 100")
+    assert len(result) == 3
